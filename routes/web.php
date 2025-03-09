a5ee23eb622a308baa175d44494dd2d19ac0a666
reda193 <mohamed.reda33@outlook.com>
2025-03-09 16:25:11 -0400

    Fixed this bug by adding middleware and aswell as adding routes

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,10 @@ use App\Http\Controllers\TodoController;
 Route::get('/', function () {
     return view('layouts.app');
 });
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+});
