c03323e6767faeda0050bea0918fb38946865ada
reda193 <mohamed.reda33@outlook.com>
2025-03-10 13:51:06 -0400

    FOr some reason when Im logged in and I try to redirect to /register it does not go to /manage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
                             <a class="nav-link" href="{{ route('login') }}">Login</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">Register</a>
+                            <a class="nav-link" href="{{ route('register') }}">Registr</a>
                         </li>
                     @endauth
                 </ul>
