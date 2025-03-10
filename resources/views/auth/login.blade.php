44679e5a7c2ef3e29ecbe4b6c17500614ed011b8
reda193 <mohamed.reda33@outlook.com>
2025-03-10 13:29:38 -0400

    Added login blade page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,24 +4,14 @@
 <div class="row justify-content-center">
     <div class="col-md-6">
         <div class="card">
-            <div class="card-header">Register</div>
+            <div class="card-header">Login</div>
             <div class="card-body">
-                <form method="POST" action="{{ url('/register') }}">
+                <form method="POST" action="{{ url('/login') }}">
                     @csrf
 
                     <div class="mb-3">
-                        <label for="name" class="form-label">Name</label>
-                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                        @error('name')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-
-                    <div class="mb-3">
                         <label for="email" class="form-label">Email Address</label>
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                         @error('email')
                             <div class="invalid-feedback">
                                 {{ $message }}
@@ -31,7 +21,7 @@
 
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                         @error('password')
                             <div class="invalid-feedback">
                                 {{ $message }}
@@ -41,12 +31,12 @@
 
                     <div class="mb-3">
                         <button type="submit" class="btn btn-primary">
-                            Register
+                            Login
                         </button>
                     </div>
 
                     <div class="mb-0">
-                        <p>Already have an account? <a href="{{ route('login') }}">Login here</a></p>
+                        <p>Don't have an account? <a href="{{ route('register') }}">Register here</a></p>
                     </div>
                 </form>
             </div>
